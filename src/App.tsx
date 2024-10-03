3fad12fbffad4860831ceb8bcc70e77930e76683
Zach Folan <zfolan@yahoo.com>
2024-10-03 16:05:44 -0700

    change to ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <hr />
+      <h1>Laminar</h1>
+      <hr />
+      <h4>Data driven, AI powered analytics</h4>
+    </>
+  )
+}
+
+export default App

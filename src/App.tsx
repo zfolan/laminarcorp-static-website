8bcbf158121235803740b2d974d2ea221808ad3f
Zach Folan <zfolan@yahoo.com>
2024-10-04 21:02:35 -0700

    update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 
+
 function App() {
   return (
     <>
@@ -7,6 +8,8 @@ function App() {
       <h1>LAMINAR</h1>
       <hr />
       <h4>Data driven, AI powered</h4>
+      <hr />
+      <h4>Callums Gay, Nats Less Gay</h4>
     </>
   )
 }

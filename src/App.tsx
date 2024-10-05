b7453240ac9881a5a29ae8f3f2f104eea5407fc0
Zach Folan <zfolan@yahoo.com>
2024-10-04 22:08:31 -0700

    revert callum change

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,9 @@ function App() {
       <h1>LAMINAR</h1>
       <hr />
       <h4>Data driven, AI powered</h4>
-      <hr />
-      <h4>Callums Gay, Nats Less Gay</h4>
     </>
   )
 }
 
 export default App
+localStorage
